efe28544a16642ea02c211f0698515d17d810b61
Nathan Ringo <remexre@gmail.com>
2018-01-21 14:38:43 -0600

    Now properly updates!

--- app/src/main/java/xyz/remexre/optimalschedulewidget/MainActivity.kt
+++ app/src/main/java/xyz/remexre/optimalschedulewidget/MainActivity.kt
@@ -3,27 +3,48 @@ package xyz.remexre.optimalschedulewidget
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.onUpdate()
 
-        setContentView(R.layout.widget)
-        val timeUntil = findViewById<TextView>(R.id.time_until)
-        val locationOf = findViewById<TextView>(R.id.location_of)
+        val nextMinute = GregorianCalendar().apply {
+            set(Calendar.SECOND, 0)
+            add(Calendar.MINUTE, 0)
+        }
 
-        try {
-            val tmp = getNextEvent(this)
+        this.timer.scheduleAtFixedRate(UpdateTask(this), nextMinute.time,
+                60 * 1000)
+    }
 
-            timeUntil.text = tmp.first
-            locationOf.text = tmp.second
-        } catch(e: NoEventException) {
-            // TODO
-            throw RuntimeException("TODO")
-        } catch(e: SecurityException) {
-            // TODO
-            throw RuntimeException("TODO")
+    class UpdateTask(private val mainActivity: MainActivity): TimerTask() {
+        override fun run() {
+            mainActivity.onUpdate()
+        }
+    }
+
+    private fun onUpdate() {
+        runOnUiThread {
+            setContentView(R.layout.widget)
+            val timeUntil = findViewById<TextView>(R.id.time_until)
+            val locationOf = findViewById<TextView>(R.id.location_of)
+
+            try {
+                val tmp = getNextEvent(this)
+
+                timeUntil.text = tmp.first
+                locationOf.text = tmp.second
+            } catch(e: NoEventException) {
+                // TODO
+                throw RuntimeException("TODO")
+            } catch(e: SecurityException) {
+                // TODO
+                throw RuntimeException("TODO")
+            }
         }
     }
 }
